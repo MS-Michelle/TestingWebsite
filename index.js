0c36af098458c0fb710f1a5947590c135c697525
Michelle Sialana <119643829+MS-Michelle@users.noreply.github.com>
2023-07-18 14:53:09 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+//Perfomring a task
+function greet(names, lastName) {
+    console.log('Hello ' + names + ' ' + lastName);
+}
+
+
+//Calculates a value
+function square(number){
+   return number * number;
+}
+
+
+console.log(square(2));
+    
+
